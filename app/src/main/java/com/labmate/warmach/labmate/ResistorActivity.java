cb691149bef7915804faa888c6398fd36d6b0be2
arpankundu007 <arpankundu007@gmail.com>
2016-07-23 02:10:03 +0530

    Resistance completed...Edge cases pending

--- app/src/main/java/com/labmate/warmach/labmate/ResistorActivity.java
+++ app/src/main/java/com/labmate/warmach/labmate/ResistorActivity.java
@@ -3,25 +3,38 @@ package com.labmate.warmach.labmate;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.DecimalFormat;
 
 /**
  * Created by warmach on 18/7/16.
  */
 public class ResistorActivity extends Activity {
     ImageButton firstColor, secondColor, thirdColor, fourthColor, toleranceColor;
-    Button convertResistance, getResistance;
+    ImageView  convertedFirstColor, convertedSecondColor, convertedThirdColor, convertedFourthColor, convertedToleranceColor;
+    Button convertResistance, getResistance, parseResistance;
     int firstValue = 0, secondValue = 0, thirdValue = 0, fourthValue = 0, toleranceValue = 0;
     TextView resistanceTextView, toleranceTextView;
+    EditText enteredResistanceValue, enteredResistancePower;
+    int[] colorArray = {R.drawable.black, R.drawable.brown, R.drawable.red, R.drawable.orange, R.drawable.yellow, R.drawable.green, R.drawable.blue, R.drawable.violet,
+            R.drawable.gray, R.drawable.white, R.drawable.gold};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +42,8 @@ public class ResistorActivity extends Activity {
         bindViews();
         setListeners();
     }
-    public void bindViews(){
+
+    public void bindViews() {
         firstColor = (ImageButton) findViewById(R.id.first);
         secondColor = (ImageButton) findViewById(R.id.second);
         thirdColor = (ImageButton) findViewById(R.id.third);
@@ -39,9 +53,17 @@ public class ResistorActivity extends Activity {
         getResistance = (Button) findViewById(R.id.show_resistance);
         resistanceTextView = (TextView) findViewById(R.id.resistance_value);
         toleranceTextView = (TextView) findViewById(R.id.tolerance_value);
+        enteredResistanceValue = (EditText) findViewById(R.id.resistance_entered);
+        enteredResistancePower = (EditText) findViewById(R.id.resistance_power);
+        parseResistance = (Button) findViewById(R.id.parse_resistance);
+        convertedFirstColor = (ImageView) findViewById(R.id.converted_first);
+        convertedSecondColor = (ImageView) findViewById(R.id.converted_second);
+        convertedThirdColor = (ImageView) findViewById(R.id.converted_third);
+        convertedFourthColor = (ImageView) findViewById(R.id.converted_fourth);
+        convertedToleranceColor = (ImageView) findViewById(R.id.converted_tolerance);
     }
 
-    public void setListeners(){
+    public void setListeners() {
         firstColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,9 +106,15 @@ public class ResistorActivity extends Activity {
                 calculateResistance();
             }
         });
+        parseResistance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parseResistance();
+            }
+        });
     }
 
-    public void showDialog(final ImageButton imageButton){
+    public void showDialog(final ImageButton imageButton) {
         LayoutInflater inflater = getLayoutInflater();
         final View colorPopup = inflater.inflate(R.layout.color_picker_alert, null);
         final AlertDialog alertDialog = new AlertDialog.Builder(this).
@@ -102,15 +130,15 @@ public class ResistorActivity extends Activity {
                     public void onClick(View view) {
                         int color = colorSpinner.getSelectedItemPosition();
                         setColor(color, imageButton);
-                        if(imageButton == firstColor)
+                        if (imageButton == firstColor)
                             firstValue = color;
-                        else if(imageButton == secondColor)
+                        else if (imageButton == secondColor)
                             secondValue = color;
-                        else if(imageButton == thirdColor)
+                        else if (imageButton == thirdColor)
                             thirdValue = color;
-                        else if(imageButton == fourthColor)
+                        else if (imageButton == fourthColor)
                             fourthValue = color;
-                        else if(imageButton == toleranceColor)
+                        else if (imageButton == toleranceColor)
                             toleranceValue = color;
                         alertDialog.dismiss();
                     }
@@ -126,10 +154,10 @@ public class ResistorActivity extends Activity {
         alertDialog.show();
     }
 
-    public void setColor(int color, ImageButton imageButton){
-        switch (color){
+    public void setColor(int color, ImageButton imageButton) {
+        switch (color) {
             case 0:
-                if(imageButton == firstColor && color == 0)
+                if (imageButton == firstColor && color == 0)
                     Toast.makeText(getBaseContext(), "First band cannot be black", Toast.LENGTH_LONG).show();
                 else
                     imageButton.setBackgroundColor(Color.BLACK);
@@ -169,7 +197,7 @@ public class ResistorActivity extends Activity {
         }
     }
 
-    public void showConversionDialog(){
+    public void showConversionDialog() {
         LayoutInflater inflater = getLayoutInflater();
         final View conversionDialogView = inflater.inflate(R.layout.resistance_conversion_dialog, null);
         final AlertDialog alertDialog = new AlertDialog.Builder(this).
@@ -190,9 +218,46 @@ public class ResistorActivity extends Activity {
     }
 
     public void calculateResistance() {
-        String resistance = "" + firstValue + "" + secondValue + "" + thirdValue + " x 10^" + fourthValue + " Ohms" ;
+        String resistance = "" + firstValue + "" + secondValue + "" + thirdValue + " x 10^" + fourthValue + " Ohms";
         String tolerance = " " + toleranceValue + " % Tol";
         resistanceTextView.setText(resistance);
         toleranceTextView.setText(tolerance);
     }
+
+    public void parseResistance() {
+        String resistance = enteredResistanceValue.getText().toString();
+        String power = enteredResistancePower.getText().toString();
+        float resistanceFloat = Float.parseFloat(resistance);
+        float powerFloat = Float.parseFloat(power);
+        String parsedResistance;
+        int zerosLeft;
+        if(powerFloat >= 3) {
+            parsedResistance = new DecimalFormat("##").format((float) (resistanceFloat * Math.pow(10, 3)));
+            zerosLeft = (int) (powerFloat - 3);
+        }
+        else {
+            parsedResistance = new DecimalFormat("##").format((float) (resistanceFloat * Math.pow(10, powerFloat)));
+            zerosLeft = 0;
+        }
+        while(zerosLeft > 1){
+            parsedResistance = parsedResistance + "0";
+            zerosLeft -- ;
+        }
+        parsedResistance = parsedResistance.replace(".","");
+        setResistanceColors(parsedResistance);
+    }
+
+    public void setResistanceColors(String resistanceValue) {
+        int col_1 = Integer.parseInt(String.valueOf(resistanceValue.charAt(0)));
+        int col_2 = Integer.parseInt(String.valueOf(resistanceValue.charAt(1)));
+        int col_3 = Integer.parseInt(String.valueOf(resistanceValue.charAt(2)));
+        int col_4 = resistanceValue.length() - 3;
+        if(col_4 > 11)
+            Toast.makeText(getBaseContext(), "Out of bounds exception", Toast.LENGTH_LONG).show();
+        convertedFirstColor.setImageResource(colorArray[col_1]);
+        convertedSecondColor.setImageResource(colorArray[col_2]);
+        convertedThirdColor.setImageResource(colorArray[col_3]);
+        convertedFourthColor.setImageResource(colorArray[col_4]);
+    }
+
 }
