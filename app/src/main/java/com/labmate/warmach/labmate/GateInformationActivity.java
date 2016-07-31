d7d7264468dff2d446a56cd64b3c67e7b84443ba
arpankundu007 <arpankundu007@gmail.com>
2016-07-31 16:34:36 +0530

    TTL info added...CMOS pending

--- app/src/main/java/com/labmate/warmach/labmate/GateInformationActivity.java
+++ app/src/main/java/com/labmate/warmach/labmate/GateInformationActivity.java
@@ -1,10 +1,19 @@
 package com.labmate.warmach.labmate;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by warmach on 30/7/16.
@@ -14,8 +23,10 @@ public class GateInformationActivity extends Activity {
     TextView gateNameTextView;
     ImageView gateTruthImageView, gateImageView;
     Intent intent;
+    Button showPinOutButton;
     int[] gate_images = {R.drawable.and_gate, R.drawable.or_gate, R.drawable.xor_gate, R.drawable.nand_gate, R.drawable.nor_gate, R.drawable.xnor_gate, R.drawable.not_gate};
     int[] truth_images = {R.drawable.and_truth, R.drawable.or_truth, R.drawable.xor_truth, R.drawable.nand_truth, R.drawable.nor_truth, R.drawable.xnor_truth, R.drawable.not_truth};
+    int[] ttl_pinouts = {R.drawable.ttl_and, R.drawable.ttl_or, R.drawable.ttl_xor, R.drawable.ttl_nand, R.drawable.ttl_nor, R.drawable.ttl_xnor, R.drawable.ttl_not};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +34,23 @@ public class GateInformationActivity extends Activity {
         intent = getIntent();
         bindViews();
         setGateName();
+        setListeners();
     }
 
     public void bindViews() {
         gateNameTextView = (TextView) findViewById(R.id.gate_name_textview);
         gateTruthImageView = (ImageView) findViewById(R.id.gate_truth_table_imageview);
         gateImageView = (ImageView) findViewById(R.id.gate_imageview);
+        showPinOutButton = (Button) findViewById(R.id.show_pin_diagram_button);
+    }
+
+    public void setListeners() {
+        showPinOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPinTypePopup();
+            }
+        });
     }
 
     public void setGateName() {
@@ -70,6 +92,98 @@ public class GateInformationActivity extends Activity {
                 gateTruthImageView.setBackgroundResource(truth_images[6]);
                 break;
         }
+    }
+
+    public void showPinTypePopup() {
+        LayoutInflater inflater = getLayoutInflater();
+        final View view = inflater.inflate(R.layout.pin_out_type_popup, null);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).setCancelable(true).create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                final RadioButton ttl = (RadioButton) view.findViewById(R.id.ttl_radioButton);
+                final RadioButton cmos = (RadioButton) view.findViewById(R.id.cmos_radioButton);
+                Button confirm = (Button) view.findViewById(R.id.chip_type_button);
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(ttl.isChecked())
+                        {
+                            alertDialog.dismiss();
+                            showTtlPopup();
+                        }
+                        else if(cmos.isChecked())
+                        {
+                            alertDialog.dismiss();
+                            showCmosPopup();
+                        }
+                        else
+                            Toast.makeText(GateInformationActivity.this, "Please select an option", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+        alertDialog.show();
+    }
+
+    public void showTtlPopup(){
+        LayoutInflater inflater = getLayoutInflater();
+        final View view = inflater.inflate(R.layout.pinout_popup, null);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).setView(view).setCancelable(true).create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                TextView chipname = (TextView) view.findViewById(R.id.chip_name_textview);
+                ImageView chipPinImage = (ImageView) view.findViewById(R.id.pin_imageview);
+                Button close = (Button) view.findViewById(R.id.close_chip_button);
+                setTtlDetails(chipname, chipPinImage);
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+            }
+        });
+        alertDialog.show();
+    }
+
+    public void setTtlDetails(TextView chipName, ImageView chipPinImage) {
+        String chipType = intent.getStringExtra("gate_type");
+        switch (chipType){
+            case "and":
+                chipName.setText("74LS08");
+                chipPinImage.setBackgroundResource(ttl_pinouts[0]);
+                break;
+            case "or":
+                chipName.setText("74LS32");
+                chipPinImage.setBackgroundResource(ttl_pinouts[1]);
+                break;
+            case "xor":
+                chipName.setText("74LS86");
+                chipPinImage.setBackgroundResource(ttl_pinouts[2]);
+                break;
+            case "nand":
+                chipName.setText("74LS00");
+                chipPinImage.setBackgroundResource(ttl_pinouts[3]);
+                break;
+            case "nor":
+                chipName.setText("74LS02");
+                chipPinImage.setBackgroundResource(ttl_pinouts[4]);
+                break;
+            case "xnor":
+                chipName.setText("74LS266");
+                chipPinImage.setBackgroundResource(ttl_pinouts[5]);
+                break;
+            case "not":
+                chipName.setText("74LS04");
+                chipPinImage.setBackgroundResource(ttl_pinouts[6]);
+                break;
+        }
+    }
+
+    public void showCmosPopup() {
+
     }
 
 
