833f4de368be2b458102befa4ebeecfbe97e5eea
arpankundu007 <arpankundu007@gmail.com>
2016-08-05 00:26:32 +0530

    opamp configs complete

--- app/src/main/java/com/labmate/warmach/labmate/AnalogComponentsActivity.java
+++ app/src/main/java/com/labmate/warmach/labmate/AnalogComponentsActivity.java
@@ -2,13 +2,23 @@ package com.labmate.warmach.labmate;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.File;
 
 /**
  * Created by warmach on 3/8/16.
@@ -45,6 +55,7 @@ public class AnalogComponentsActivity extends Activity {
             public void onShow(DialogInterface dialogInterface) {
                 Button inverting = (Button) view.findViewById(R.id.inverting_button);
                 Button nonInverting = (Button) view.findViewById(R.id.non_inverting_button);
+                final Button downloadDatasheet = (Button) view.findViewById(R.id.opamp_datasheet_button);
                 inverting.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -59,6 +70,12 @@ public class AnalogComponentsActivity extends Activity {
                         navigateToOpampConfig("non-inverting");
                     }
                 });
+                downloadDatasheet.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        downloadDatasheet();
+                    }
+                });
             }
         });
         alertDialog.show();
@@ -68,5 +85,64 @@ public class AnalogComponentsActivity extends Activity {
         Intent intent = new Intent(AnalogComponentsActivity.this, OpampConfigurationActivity.class);
         intent.putExtra("config", config);
         startActivity(intent);
+    }
+
+    public void downloadDatasheet() {
+        String url = "http://www.ti.com/lit/ds/symlink/lm741.pdf";
+        String[] fileName = url.split("/");
+        String name = fileName[fileName.length - 1];
+        Log.v("File Name:", name);
+        File file = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
+        File list[] = file.listFiles();
+        int exists = 0;
+        for(int i=0 ; i < list.length ; i++){
+            if(list[i].getName().equals(name))
+            {
+                openFile(list[i]);
+                exists = 1;
+            }
+        }
+        if(exists == 1)
+            Toast.makeText(getBaseContext(), "File Exists", Toast.LENGTH_SHORT).show();
+        else if(isNetworkAvailable(this)) {
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        else
+            Toast.makeText(getBaseContext(), "Please check your Internet Connection", Toast.LENGTH_SHORT).show();
+    }
+
+    public void openFile(File file){
+        Intent target = new Intent(Intent.ACTION_VIEW);
+        target.setDataAndType(Uri.fromFile(file),"application/pdf");
+        target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        Intent intent = Intent.createChooser(target, "Open File");
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getBaseContext(), "No PDF reader found..Please install one from the playstore", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        boolean status = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getNetworkInfo(0);
+            if (netInfo != null && netInfo.getState() == NetworkInfo.State.CONNECTED) {
+                status = true;
+            }
+            else {
+                netInfo = cm.getNetworkInfo(1);
+                if (netInfo != null && netInfo.getState() == NetworkInfo.State.CONNECTED)
+                    status = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return status;
     }
 }
--- app/src/main/java/com/labmate/warmach/labmate/OpampConfigurationActivity.java
+++ app/src/main/java/com/labmate/warmach/labmate/OpampConfigurationActivity.java
@@ -3,20 +3,29 @@ package com.labmate.warmach.labmate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.DecorContentParent;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 /**
  * Created by warmach on 3/8/16.
  */
 public class OpampConfigurationActivity extends Activity {
     Intent intent;
-    TextView configTypeTextView;
     ImageView invertingImageView;
     Spinner configSpinner;
+    Button gainButton;
+    EditText r1EditText, r1PowerEditText, r2EditText, r2PowerEditText;
+    TextView gainTextView, gainFormulaTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +36,21 @@ public class OpampConfigurationActivity extends Activity {
     }
 
     public void bindViews() {
+        gainFormulaTextView = (TextView) findViewById(R.id.gain_formula_textview);
+        gainTextView = (TextView) findViewById(R.id.gain_value_textview);
         invertingImageView = (ImageView) findViewById(R.id.config_type_imageview);
         configSpinner = (Spinner) findViewById(R.id.config_spinner);
-        if(intent.getStringExtra("config").equals("inverting"))
+        if(intent.getStringExtra("config").equals("inverting")) {
             setInverting();
-        else if(intent.getStringExtra("config").equals("non-inverting"))
+        }
+        else if(intent.getStringExtra("config").equals("non-inverting")) {
             setNonInverting();
+        }
+        gainButton = (Button) findViewById(R.id.gain_button);
+        r1EditText = (EditText) findViewById(R.id.r1_editText);
+        r1PowerEditText = (EditText) findViewById(R.id.r1_power_editText);
+        r2EditText = (EditText) findViewById(R.id.r2_edittext);
+        r2PowerEditText = (EditText) findViewById(R.id.r2_power_edittext);
     }
 
     public void setListeners() {
@@ -50,15 +68,77 @@ public class OpampConfigurationActivity extends Activity {
 
             }
         });
+        gainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (setValidations()) {
+                    if (configSpinner.getSelectedItemPosition() == 0)
+                        calculateInvertingGain();
+                    else
+                        calculateNonInvertingGain();
+                }
+            }
+        });
     }
 
     public void setInverting() {
         configSpinner.setSelection(0);
         invertingImageView.setBackgroundResource(R.drawable.inverting);
+        gainTextView.setText("");
+        gainFormulaTextView.setText("(-R2/R1)");
     }
 
     public void setNonInverting() {
         configSpinner.setSelection(1);
         invertingImageView.setBackgroundResource(R.drawable.non_inverting);
+        gainTextView.setText("");
+        gainFormulaTextView.setText("(1 + (R2/R1))");
+    }
+
+    public void calculateInvertingGain() {
+        String r1 = r1EditText.getText().toString();
+        String r1_power = r1PowerEditText.getText().toString();
+        String r2 = r2EditText.getText().toString();
+        String r2_power = r2PowerEditText.getText().toString();
+        DecimalFormat df = new DecimalFormat("#.###");
+        if(Float.parseFloat(r1) == 0)
+            Toast.makeText(getBaseContext(), "R1 cannot be zero", Toast.LENGTH_LONG).show();
+        else {
+            float div = Float.parseFloat(r2) / Float.parseFloat(r1);
+            int sub = (int) (Float.parseFloat(r2_power) - Float.parseFloat(r1_power));
+            String gain = "";
+            if (sub == 0)
+                gain = "" + df.format(div);
+            else
+                gain = "" + df.format(div * Math.pow(10, sub));
+            gainTextView.setText("- " + gain);
+        }
+    }
+
+    public void calculateNonInvertingGain() {
+        String r1 = r1EditText.getText().toString();
+        String r1_power = r1PowerEditText.getText().toString();
+        String r2 = r2EditText.getText().toString();
+        String r2_power = r2PowerEditText.getText().toString();
+        DecimalFormat df = new DecimalFormat("#.###");
+        if(Float.parseFloat(r1) == 0)
+            Toast.makeText(getBaseContext(), "R1 cannot be zero", Toast.LENGTH_LONG).show();
+        else {
+            float div = Float.parseFloat(r2) / Float.parseFloat(r1);
+            int sub = (int) (Float.parseFloat(r2_power) - Float.parseFloat(r1_power));
+            float temp = (float) (div * Math.pow(10, sub));
+            float gain = 1 + temp;
+            gainTextView.setText("" + df.format(gain));
+        }
+    }
+
+    public boolean setValidations() {
+        if(r1EditText.getText().toString().length() == 0 || r1PowerEditText.getText().toString().length() == 0 ||
+                r2EditText.getText().toString().length() == 0 || r2PowerEditText.getText().toString().length() == 0)
+        {
+            Toast.makeText(getBaseContext(), "Please enter the values", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
