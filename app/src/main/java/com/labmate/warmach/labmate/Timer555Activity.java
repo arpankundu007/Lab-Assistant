894f6fc903e8a64ac0cb95035c1f4c5820ed0ac0
arpankundu007 <arpankundu007@gmail.com>
2016-08-06 03:15:31 +0530

    Minor Changes

--- app/src/main/java/com/labmate/warmach/labmate/Timer555Activity.java
+++ app/src/main/java/com/labmate/warmach/labmate/Timer555Activity.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,6 +94,8 @@ public class Timer555Activity extends Activity {
                     else
                         setMonoStableModeCalculation();
                     ObjectAnimator.ofInt(scrollView, "scrollY", scrollView.getScrollY() + 1000).setDuration(1000).start();
+                    InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
                 }
             }
         });
